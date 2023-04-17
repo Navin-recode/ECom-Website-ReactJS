0a64a9aec31b1cb277dbffb2323695d7aec3eabe
navin kukreja <navinexpo2020@gmail.com>
2023-04-17 23:49:46 +0530

    worked on Signle Product Page

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,18 +3,150 @@ import Meta from '../components/Meta'
 import BreadCrumb from '../components/BreadCrumb'
 import ProductCard from '../components/ProductCard'
 import ReactStars from 'react-rating-stars-component'
+import ReactImageZoom from 'react-image-zoom'
+import Color from '../components/Color'
+import { Link } from 'react-router-dom'
+// import headphone from '../../public/images/images/headphone.jpg'
 
 const SingleProduct = () => {
+  const props = {
+    width: 600,
+    height: 600,
+    zoomWidth: 600,
+    img:
+      'https://m.media-amazon.com/images/I/71IGPk09H-L._AC_UF894,1000_QL80_.jpg',
+  }
+
   const [orderedProduct, setOrderedProduct] = useState(true)
   return (
     <>
       <Meta title={'Product Name'} />
       <BreadCrumb title="Product Name" />
+
       <div className="main-product-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="main-product-image">
+                <div>
+                  <ReactImageZoom {...props} />
+                </div>
+                <div className="other-product-images d-flex flex-wrap gap-15">
+                  <div>
+                    <img
+                      src="https://m.media-amazon.com/images/I/71IGPk09H-L._AC_UF894,1000_QL80_.jpg"
+                      className="img-fluid"
+                      alt="watch"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      src="https://m.media-amazon.com/images/I/71IGPk09H-L._AC_UF894,1000_QL80_.jpg"
+                      className="img-fluid"
+                      alt="watch"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      src="https://m.media-amazon.com/images/I/71IGPk09H-L._AC_UF894,1000_QL80_.jpg"
+                      className="img-fluid"
+                      alt="watch"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      src="https://m.media-amazon.com/images/I/71IGPk09H-L._AC_UF894,1000_QL80_.jpg"
+                      className="img-fluid"
+                      alt="watch"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="main-product-details">
+                <div className="border-bottom">
+                  <h3>Kids Headphonse bulk pack 10 Multi color students</h3>
+                </div>
+                <div className="border-bottom py-3">
+                  <p className="price">$ 100</p>
+                  <div className="d-flex align-items-center gap-10">
+                    <ReactStars
+                      count={5}
+                      size={24}
+                      value={4}
+                      edit={true}
+                      activeColor="#ffd700"
+                    />
+                    <p className="mb-0 t-review"> ( 2 Reviews )</p>
+                  </div>
+                  <a className="review-btn" href="#review">
+                    Write a Review
+                  </a>
+                </div>
+                <div className="border-bottom">
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Type :</h3>
+                    <p className="product-data">Watch</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Brand :</h3>
+                    <p className="product-data">Boat</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Category :</h3>
+                    <p className="product-data">Watch</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Tags :</h3>
+                    <p className="product-data">Watch</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Availability :</h3>
+                    <p className="product-data">In Stock</p>
+                  </div>
+                  <div className="d-flex gap-10 flex-column mt-2 mb-3">
+                    <h3 className="product-heading">Size :</h3>
+                    <div className="d-flex flex-wrap gap-15">
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        S
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        M
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XL
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XXL
+                      </span>
+                    </div>
+                  </div>
+                  <div className="d-flex gap-10 flex-column  mt-2 mb-3">
+                    <h3 className="product-heading">Color :</h3> 
+                    <Color />
+                  </div>
+                  <div className="d-flex align-items-center gap-15 flex-row mt-2 mb-3">
+                    <h3 className="product-heading">Quantity :</h3>
+                    <div className="">
+                      <input
+                        type="number"
+                        name=""
+                        min={0}
+                        max={10}
+                        style={{ width: '70px' }}
+                        id=""
+                        className="form-control"
+                      />
+                    </div>
+                    <div className='d-flex align-items-center gap-30 ms-5'>
+                    <button className="button border-0" type='submit'>Add To Cart</button>
+                    <button className="button signup">Buy It Now </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -43,7 +175,7 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <h3>Reviewes</h3>
+              <h3 id="review">Reviewes</h3>
               <div className="review-inner-wrapper">
                 <div className="review-head d-flex justify-content-between align-items-end">
                   <div>
